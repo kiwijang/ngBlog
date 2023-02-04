4fa4f7786d79233bd583930cc6db91e9684d9b4a
naomi <naomi@miniasp.com>
2023-02-04 17:17:59 +0800

    Initial commit

---
+++ apps/ng-blog/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-blog-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ng-blog';
+}
